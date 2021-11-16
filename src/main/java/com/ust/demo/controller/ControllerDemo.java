1c4094fd0e0ce7254a6531d27f5a501e1ad21322
Aparna.Babu1 <Aparna.Babu1@ust.com>
2021-11-16 13:25:19 +0530

    second

--- src/main/java/com/ust/demo/controller/ControllerDemo.java
+++ src/main/java/com/ust/demo/controller/ControllerDemo.java
@@ -1,7 +1,5 @@
 package com.ust.demo.controller;
 
-import java.net.http.HttpResponse;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerDemo {
-	
+	//created getmapping request
 	@GetMapping("/app/welcome")
 	public ResponseEntity<String> welcome(){
 		return new ResponseEntity<>("Hi welcome to springboot app",HttpStatus.OK);
